7e4b5bdc60cc8fc2e8f9a39dc1db5aa1a53e7c3f
randolphcyg <cyg0054@gmail.com>
2023-04-23 16:56:29 +0800

    first commit

---
+++ src/views/Introduce.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-card class="account-container">
+    <h1>系统简介</h1>
+    <div style="line-height: 30px">
+      vue3-admin 是一套企业级后台管理系统，基于 Spring Boot 、Vue 3.0 相关技术栈开发，供各位开发者们体验和学习，一定给你最好的学习体验。
+    </div>
+    <h1>开发及部署文档</h1>
+    <img style="width: 70%;
+      display: block;
+      margin: 0 auto;" src="https://s.yezgea02.com/1616938239101/419241616938196_.pic.jpg" />
+    <h1>技术选型</h1>
+    <ul style="font-weight: bold;">
+      <li>Vue 3.x</li>
+      <li>Vite 2.x</li>
+      <li>Vue-Router 4.x</li>
+      <li>Element-Plus（适配全新 Vue 3.x 的 PC 端组件库）</li>
+      <li>Echarts 5.0</li>
+      <li>axios</li>
+      <li>wangEditor</li>
+    </ul>
+  </el-card>
+</template>
+
+<style scoped>
+  .course li {
+    line-height: 36px;
+    color: #409eff;
+    cursor: pointer;
+  }
+</style>
